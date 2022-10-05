f9e0857a903ae811c4c474249d9aad169a60407c
OkaAditya <felixoka@gmail.com>
2022-10-05 20:13:25 +0800

    adding back button handler at home page

--- lib/page/home_page/home_page.dart
+++ lib/page/home_page/home_page.dart
@@ -1,6 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'home_cubit.dart';
@@ -14,135 +15,153 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HomeCubit()..homeInit(),     
-      child: Scaffold(
-        body: BlocConsumer<HomeCubit, HomeState>(
-          listener: (context, state){
-            if(state is HomeStateError){
-              errorAlert(state.message, context);
-            }
-          },
-          builder: (context, state){
-            if(state is HomeStateLoading){
-              return Center(
-                child: loadingWidget(context),
-              );
-            }else if(state is HomeStateComplated){
-              return Scaffold(
-                appBar: AppBar(
-                  leading: Container(),
-                  title: Text(
-                    'TO DO',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  actions: [Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: SizeConfig.sizeHorizontal(2),
-                    ),
-                    child: IconButton(
-                      icon: FaIcon(
-                        FontAwesomeIcons.rightFromBracket,
+    DateTime backPressed = DateTime.now();
+    return WillPopScope(
+      onWillPop: () async{
+        final cantExit = DateTime.now().difference(backPressed) >= const Duration(seconds: 2);
+        backPressed = DateTime.now();
+        if(cantExit){
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Press once again to exit'),
+              
+            ),
+          );
+          return false;
+        } else {
+          exit(0);
+        }
+      },
+      child: BlocProvider(
+        create: (context) => HomeCubit()..homeInit(),     
+        child: Scaffold(
+          body: BlocConsumer<HomeCubit, HomeState>(
+            listener: (context, state){
+              if(state is HomeStateError){
+                errorAlert(state.message, context);
+              }
+            },
+            builder: (context, state){
+              if(state is HomeStateLoading){
+                return Center(
+                  child: loadingWidget(context),
+                );
+              }else if(state is HomeStateComplated){
+                return Scaffold(
+                  appBar: AppBar(
+                    leading: Container(),
+                    title: Text(
+                      'TO DO',
+                      style: TextStyle(
                         color: Theme.of(context).primaryColor,
                       ),
-                      onPressed: () => logOut(context),
                     ),
-                  )],
-                ),
-                body: StreamBuilder(
-                  stream: state.data.snapshots(),
-                  builder: (context, snapshot){
-                    if(snapshot.connectionState == ConnectionState.waiting){
-                      return Center(
-                        child: loadingWidget(context),
-                      );
-                    } else{
-                      if(snapshot.hasError){
+                    actions: [Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: SizeConfig.sizeHorizontal(2),
+                      ),
+                      child: IconButton(
+                        icon: FaIcon(
+                          FontAwesomeIcons.rightFromBracket,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () => logOut(context),
+                      ),
+                    )],
+                  ),
+                  body: StreamBuilder(
+                    stream: state.data.snapshots(),
+                    builder: (context, snapshot){
+                      if(snapshot.connectionState == ConnectionState.waiting){
                         return Center(
-                          child: Text('snapshot error = ${snapshot.error}'),
+                          child: loadingWidget(context),
                         );
-                      }else if(snapshot.hasData){
-                        if(snapshot.data!.size == 0){
-                          return const Center(
-                            child: Text('No Todo'),
+                      } else{
+                        if(snapshot.hasError){
+                          return Center(
+                            child: Text('snapshot error = ${snapshot.error}'),
                           );
-                        } else{
-                          return ListView(
-                            children: snapshot.data!.docs.map((DocumentSnapshot document){
-                              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                              return Container(
-                                margin: EdgeInsets.symmetric(
-                                  vertical: SizeConfig.sizeVertical(1),
-                                  horizontal: SizeConfig.sizeHorizontal(1),
-                                ),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: Theme.of(context).appBarTheme.backgroundColor,
-                                ),
-                                child: ListTile(
-                                  leading: InkWell(
-                                    onTap: () =>context.read<HomeCubit>().changeStatusTodo(
-                                      document.id, data['finish']
-                                    ),
-                                    child: Container(
-                                      alignment: Alignment.center,
-                                      height: SizeConfig.sizeVertical(4),
-                                      width: SizeConfig.sizeVertical(4),
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: (data['finish'])
-                                            ? Theme.of(context).primaryColor
-                                            : const Color(0xff4C566A),
+                        }else if(snapshot.hasData){
+                          if(snapshot.data!.size == 0){
+                            return const Center(
+                              child: Text('No Todo'),
+                            );
+                          } else{
+                            return ListView(
+                              children: snapshot.data!.docs.map((DocumentSnapshot document){
+                                Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                                return Container(
+                                  margin: EdgeInsets.symmetric(
+                                    vertical: SizeConfig.sizeVertical(1),
+                                    horizontal: SizeConfig.sizeHorizontal(1),
+                                  ),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Theme.of(context).appBarTheme.backgroundColor,
+                                  ),
+                                  child: ListTile(
+                                    leading: InkWell(
+                                      onTap: () =>context.read<HomeCubit>().changeStatusTodo(
+                                        document.id, data['finish']
+                                      ),
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        height: SizeConfig.sizeVertical(4),
+                                        width: SizeConfig.sizeVertical(4),
+                                        decoration: BoxDecoration(
+                                          border: Border.all(
+                                            color: (data['finish'])
+                                              ? Theme.of(context).primaryColor
+                                              : const Color(0xff4C566A),
+                                          ),
+                                          borderRadius: BorderRadius.circular(10),
                                         ),
-                                        borderRadius: BorderRadius.circular(10),
+                                        child: FaIcon(
+                                          FontAwesomeIcons.check,
+                                          color: (data['finish'])
+                                          ? Theme.of(context).primaryColor
+                                          : const Color(0xff4C566A),
+                                        ),
                                       ),
-                                      child: FaIcon(
-                                        FontAwesomeIcons.check,
-                                        color: (data['finish'])
-                                        ? Theme.of(context).primaryColor
-                                        : const Color(0xff4C566A),
+                                    ),
+                                    title: Text("${data['todo']}"),
+                                    trailing: IconButton(
+                                      onPressed: () => context.read<HomeCubit>().deleteTodo(document.id),
+                                      icon: const FaIcon(
+                                        FontAwesomeIcons.trash,
+                                        color: Color(0xffBF616A),
                                       ),
                                     ),
                                   ),
-                                  title: Text("${data['todo']}"),
-                                  trailing: IconButton(
-                                    onPressed: () => context.read<HomeCubit>().deleteTodo(document.id),
-                                    icon: const FaIcon(
-                                      FontAwesomeIcons.trash,
-                                      color: Color(0xffBF616A),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            }).toList(),
+                                );
+                              }).toList(),
+                            );
+                          }
+                        }else {
+                          return Center(
+                            child: Text('snapshot = $snapshot'),
                           );
                         }
-                      }else {
-                        return Center(
-                          child: Text('snapshot = $snapshot'),
-                        );
                       }
-                    }
-                  },
-                ),
-                floatingActionButton: FloatingActionButton(
-                  elevation: 1,
-                  onPressed: () => addTodo(context),
-                  backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-                  child: Center(
-                    child: FaIcon(
-                      FontAwesomeIcons.plus,
-                      color: Theme.of(context).primaryColor,
+                    },
+                  ),
+                  floatingActionButton: FloatingActionButton(
+                    elevation: 1,
+                    onPressed: () => addTodo(context),
+                    backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+                    child: Center(
+                      child: FaIcon(
+                        FontAwesomeIcons.plus,
+                        color: Theme.of(context).primaryColor,
+                      ),
                     ),
                   ),
-                ),
-              );
-            }else{
-              return Container();
-            }
-          },
+                );
+              }else{
+                return Container();
+              }
+            },
+          ),
         ),
       ),
     );
